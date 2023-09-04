1e94d7aba1cb99e966bd10c34ee550e378335773
GentaProject5 <117004544+GentaProject5@users.noreply.github.com>
2023-09-05 01:04:54 +0700

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -7,12 +7,12 @@ load_dotenv()
 que = {}
 admins = {}
 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
-BOT_USERNAME = getenv("BOT_USERNAME")
-BOT_TOKEN = getenv("BOT_TOKEN")
+API_ID = int(getenv("25989336"))
+API_HASH = getenv("af53cce8dc3b6fb2e4ae0c17e9d646df")
+BOT_USERNAME = getenv("v1Bnubot")
+BOT_TOKEN = getenv("6091391494:AAHOIxGlD6tNFB3TGgKGGUlVDkjvuPNuuOo")
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "300"))
-STRING_SESSION = getenv("STRING_SESSION")
+STRING_SESSION = getenv("BQDDV4QAZcN35eLhfXbWWF-NJ5yJAgaetIlK5FMksPK1GF076VuGQ6_tY-fRbqk__Me0f0tCHrk5CtFLM8QnjUSzSVNZhlja9sQGfHW2975PYQzyocgfPD4zeIL8p2Nvcx7QNTyf3W9wVuRpUJpxUwP76WCtnsGbUmoB6L59EvZhyrZpiA9KewIQ6TCkK3QW56g89lIAn8pEMynW4sVdi_CBQ-Zn-cOTuawBSCWhmS2ASPbN6D9ZQGy2kaK_ixIMRu85lzVXTFivwitRCWyUnbz4DiI0kL3XEXvrVwLu1_y5qOKj4w743_LTTbUEY8run1hq_wcQ_o28CoDDf4puj50tGNQxiAAAAAByxTHIAA")
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/").split())
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5513481385").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1925525960").split()))
 aiohttpsession = aiohttp.ClientSession()
